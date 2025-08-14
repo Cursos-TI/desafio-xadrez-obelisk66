5cff05a6c84c2a6e127432e62a30bfca11a806f6
Halan Franklin <lanfranklinlol@gmail.com>
2025-08-14 13:06:26 +0000

    Xadrez Concluído! (Nível mestre)

--- xadrez.c
+++ xadrez.c
@@ -4,6 +4,55 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+void MovCavalo(int mov){//movimentação do cavalo
+     while (mov--)
+    {
+        for(int i = 0;i < 2; i++ ){
+            printf("Cavalo(Cima)\n");
+
+        }
+        printf("Cavalo(Direita)\n");
+    }
+}
+
+void MovRainha(int mov){//movimentação da Rainha
+     do{
+     printf("Rainha(Esquerda)\n");
+     mov--;
+    }while (mov > 0);
+    
+}
+
+void MovBispo(int mov){//movimentação do Bispo
+  for (mov ; mov > 0; mov--)
+  {
+    printf("Bispo(Cima)\n");
+    for (int i = 1; i > 0; i--)
+    {
+        printf("Bispo(Direita)\n");
+    }
+    
+  }
+  
+
+}
+
+/*movimentação da Torre.jé estudava react e java Bastante iniciante!, aí fiz essa implementação para nao copiar 4 vezes o mesmo código
+a const char*variavél eu tive que olhar no gpt, mas a chamada já sabia fazer .
+*/
+
+void MovTorre(int mov, const char*texto){
+     while (mov-- ) {
+        printf("%s \n", texto);   
+     }
+
+}
+
+
+
+
+
+
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
@@ -34,7 +83,7 @@ int torre;
 // Não entendi direito a obrigatóriedade do exercício e decidir fazer desta forma.Mas como tem uma "liberdade de exercitar o código" estou fazendo.
 
 
-// menu das peças para seelcionar
+// menu das peças
 printf("**Escolha a peça do xadrez para movimentar **\n");
 printf("1. Torre\n");
 printf("2. Bispo\n");
@@ -45,8 +94,8 @@ scanf("%d", &peca);
 
 switch (peca) // para determinar cada movimento das peças separados
 {
-case 1: // movimentaçao da torre
-    int movimento;
+case 1:
+    int movimento;  //seleção para movimentação da torre Vertical ou Horinzontal
     printf("escolha a direção da movimentação da torre:\n");
     printf("1 movimenta para horinzontal\n");
     printf("2 movimenta para Vertical\n");
@@ -55,6 +104,7 @@ case 1: // movimentaçao da torre
     if (movimento == 1)
     {
         char movimento2;
+         //seleção para movimentação da torre Esquerda ou Direita
         printf("movimentando torre para horizontal\n");
         printf("Para esquerda use o E e para  Direita use o D :\n");
         scanf("%s",&movimento2);
@@ -63,29 +113,21 @@ case 1: // movimentaçao da torre
          {
          case 'e':
          case 'E':{
-         int i;
-         int mov = 0;
-            printf("escolha quantas casas\n");
-            scanf("%d",&i);
-            while (mov < i )
-            {
-                printf("Esquerda\n");
-                mov++;
-            }
+        
+            int mov;
+            printf("escolha quantas casas:\n");
+            scanf("%d",&mov);
+            MovTorre(mov,"Torre(Esquerda)");// troca do loop por código Recursivo 
         
             break;
         }
             case 'd':
          case 'D':{
-         int i;
-         int mov = 0;
-            printf("escolha quantas casas\n");
-            scanf("%d",&i);
-            while (mov < i )
-            {
-                printf("Direita\n");
-                mov++;
-            }
+         
+         int mov;
+            printf("escolha quantas casas:\n");
+            scanf("%d",&mov);
+            MovTorre(mov,"Torre(Direita)");// troca do loop por código Recursivo 
             
             break;
         }
@@ -103,77 +145,59 @@ case 1: // movimentaçao da torre
          {
          case 'c':
          case 'C':{
-         int i;
-         int mov = 0;
-            printf("escolha quantas casas\n");
-            scanf("%d",&i);
-            while (mov < i )
-            {
-                printf("Cima\n");
-                mov++;
-            }
+
+            int mov;
+            printf("escolha quantas casas:\n");
+            scanf("%d",&mov);
+            MovTorre(mov,"Torre(Cima)");// troca do loop por código Recursivo 
         
             break;
         }
-            case 'b':
+         case 'b':
          case 'B':{
-         int i;
-         int mov = 0;
-            printf("escolha quantas casas\n");
-            scanf("%d",&i);
-            while (mov < i )
-            {
-                printf("Baixo\n");
-                mov++;
-            }
+
+           int mov;
+           printf("escolha quantas casas:\n");
+           scanf("%d",&mov);
+           MovTorre(mov,"Torre(Baixo)");// troca do loop por código Recursivo 
             
             break;
         }
          default:
             break;
-         }
+        }
 
 
     }
     
 
     break;
-case 2://movimentação do Bispo
+case 2:
 // fiquei sem tempo pra terminar o restante .
+int casa ;
+// pensei em implentar igual a torre, só que o código iria ficar muito longo
+
   printf("escolha a direção da movimentação do Bispo:\n");
-  printf("Escolha prédefinida pelo professor .:\n");
-  for (int i = 0; i < 5; i++)
-  {
-    printf("cima\n");
-    printf("direita\n");
-  }
+  printf("Escolha quantas casas o Bispo deve mover para diagonal direira (Cima) .:\n");
+  scanf("%d", &casa);
+  MovBispo(casa); // troca do loop por código Recursivo 
   
     break;
-case 3:{// Movimentação da Rainha
+case 3:{
  // fiquei sem tempo pra terminar o restante .
- int i = 0;
+ int mov = 8;
   printf("escolha a direção da movimentação do Rainha:\n");
   printf("Escolha pré definida pelo professor .:\n");
-  
-  do{
-    printf("Esquerda\n");
-    i++;
-  }while (i < 8);
+
+  MovRainha(mov);
+
   break;
 }
-case 4:{ // Movimentação do cavalo
-    int cavalo = 1;
-    printf("Cavalo Escolha o movimento do cavalo: \n");
+case 4:{
+    int mov = 1;
+    printf(" Escolha o movimento do Cavalo: \n");
     printf("escolha pré definida pelo professor.: \n");
-    while (cavalo--)
-    {
-        for(int i = 0;i < 2; i++ ){
-            printf("Baixo\n");
-
-        }
-        printf("Esquerda\n");
-    }
-    
+    MovCavalo(mov);// troca do loop por código Recursivo 
   
     break;
 }
@@ -184,8 +208,6 @@ default:
 
 
 
-
 return 0;
 
 }
-
